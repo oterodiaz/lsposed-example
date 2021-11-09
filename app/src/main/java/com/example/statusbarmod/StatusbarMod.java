38decbfe9c1caf350444d95124b8af6afce922b3
oterodiaz <44239644+oterodiaz@users.noreply.github.com>
2021-11-09 03:29:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/statusbarmod/StatusbarMod.java
@@ -0,0 +1,29 @@
+package com.example.statusbarmod;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import android.graphics.Color;
+import android.widget.TextView;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class StatusbarMod implements IXposedHookLoadPackage {
+    public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("com.android.systemui")) // Only run the module on SystemUI
+            return;
+
+        XposedBridge.log("(StatusbarMod) Loaded app: " + lpparam.packageName);
+        findAndHookMethod("com.android.systemui.statusbar.policy.Clock", lpparam.classLoader, "updateClock", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                TextView tv = (TextView) param.thisObject;
+                String text = tv.getText().toString();
+                tv.setText(String.format("The time is %s", text));
+                tv.setTextColor(Color.BLUE);
+            }
+        });
+
+        XposedBridge.log("(StatusbarMod) Clock modified");
+    }
+}
